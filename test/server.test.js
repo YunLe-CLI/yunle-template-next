0c333f2c82c68d9b335842a560002ae374aac92c
hexiao-o <460559761@qq.com>
2016-11-16 11:18:41 +0800

    加入单元测试

---
+++ test/server.test.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../index');
+var should = chai.should();
+chai.use(chaiHttp);
+describe('Blobs', function() {
+  it('should index on / GET');
+});
