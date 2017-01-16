0bf68b6ab642b8f001109c547ada36ea3d72fefa
何潇 <hexiao@hexiaodeMacBook-Pro.local>
2017-01-16 15:08:49 +0800

    暂时简写test

--- test/server.test.js
+++ test/server.test.js
@@ -1,8 +1,9 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../index');
-var should = chai.should();
-chai.use(chaiHttp);
-describe('Blobs', function() {
-  it('should index on / GET');
+var expect = require('chai').expect;
+function add(x, y) {
+  return x + y;
+}
+describe('加法函数的测试', function() {
+  it('1 加 1 应该等于 2', function() {
+    expect(add(1, 1)).to.be.equal(2);
+  });
 });
