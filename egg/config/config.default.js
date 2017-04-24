2804aa5846353cf554bc1fae9e5c39e78e80ff30
hexiao-o <460559761@qq.com>
2017-04-24 19:48:30 +0800

    add egg

---
+++ egg/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1493032834136_8896';
+
+  return config;
+};
